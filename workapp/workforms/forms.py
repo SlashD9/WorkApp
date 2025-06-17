53e5857065e6e9d6bcaa29853f08ffc194dd21ad
C Daly <colin.p.s.daly@gmail.com>
2025-06-17 08:09:59 +0100

    Update forms.py
    added forms for each

--- workapp/workforms/forms.py
+++ workapp/workforms/forms.py
@@ -7,7 +7,26 @@ class SiteForm(forms.ModelForm):
         model = Site
         fields = ["site_name","address","town","county","country","aircode"]
 
+#Customer Form
+#Requires fields to be added
+class CustomerForm(forms.ModelForm):
+    class Meta :
+        model = Customer
+        fields = ["site_name"]
 
+#Worksheet form
+#Requires fields to be added
+class WorksheetForm(forms.ModelForm):
+    class Meta :
+        model = Worksheet
+        fields = ["site_name"]
+
+#Service Form
+#Requires fields to be added
+class ServiceForm(forms.ModelForm):
+    class Meta :
+        model = Service
+        fields = ["site_name"]
 
 class StockForm(forms.ModelForm):
     class Meta :
