3f5cae09e7562a58203500bcaee8967365531f18
SlashD9 <colin.p.s.daly@gmail.com>
2025-06-18 22:44:27 +0100

    added edit to forms

--- workapp/workforms/urls.py
+++ workapp/workforms/urls.py
@@ -11,16 +11,26 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', sites, name='Sites'),
+    
     path('sites/', sites, name='Sites'),
-    path('customer/', customer, name='Customer'),
-    path('worksheet/', worksheet, name='Worksheet'),
-    path('service/', service, name='Service Sheet'),
-    path('stock/', stock, name='Stock Sheet'),
     path('sites/add_site/', views.add_site, name="add_site"),
+    path('sites/edit_site/<id>', views.edit_site, name="edit_site"),
+
+    path('customer/', customer, name='Customer'),
     path('customer/add_customer/', views.add_customer, name="add_customer"),
+    path('customer/edit_customer/<id>', views.edit_customer, name="edit_customer"),
+
+    path('worksheet/', worksheet, name='Worksheet'),
     path('worksheet/add_worksheet/', views.add_worksheet, name="add_worksheet"),
+    path('worksheet/edit_worksheet/<id>', views.edit_worksheet, name="edit_worksheet"),
+
+    path('service/', service, name='Service Sheet'),
     path('service/add_service/', views.add_service, name="add_service"),
+    path('service/edit_service/<id>', views.edit_service, name="edit_service"),
+
+    path('stock/', stock, name='Stock Sheet'),
     path('stock/add_stock/', views.add_stock, name="add_stock"),
+    path('stock/edit_stock/<id>', views.edit_stock, name="edit_stock"),
     
     
 ]
--- workapp/workforms/views.py
+++ workapp/workforms/views.py
@@ -6,25 +6,11 @@ from .forms import SiteForm, StockForm, CustomerForm, WorksheetForm, ServiceForm
 def home(request):
     return HttpResponse("<h1>Hello, from workforms</h1>")
 
+                # Sites Views #
+                ###############
 def sites(request):
     sites = Site.objects.all()
     return render(request, 'sites.html', {'sites': sites})
-
-def customer(request):
-    customers = Customer.objects.all()
-    return render(request, 'customer.html', {'customers': customers})
-
-def worksheet(request):
-    worksheets = Worksheet.objects.all()
-    return render(request, 'worksheet.html', {'worksheets': worksheets})
-
-def service(request):
-    services = Service.objects.all()
-    return render(request, 'service.html', {'services': services})
-
-def stock(request):
-    stocks = Stock.objects.all()
-    return render(request, 'stock.html', {'stocks': stocks})
 
 def add_site(request):
     if request.method =="POST":
@@ -35,6 +21,24 @@ def add_site(request):
     else:
         form = SiteForm()
         return render(request, "add_site.html",{'form': form})
+    
+def edit_site(request, id):
+    site = Site.objects.get(id=id)
+    if request.method == 'POST':
+        form = SiteForm(request.POST, instance=site)
+        if form.is_valid():
+            form.save()
+            return redirect('sites', site.id)
+    else:
+        form = SiteForm(instance=site)
+    return render(request, 'edit_site.html', {'form': form})
+
+
+                # Customer Views #
+                ##################
+def customer(request):
+    customers = Customer.objects.all()
+    return render(request, 'customer.html', {'customers': customers})
 
 def add_customer(request):
     if request.method =="POST":
@@ -45,6 +49,25 @@ def add_customer(request):
     else:
         form = CustomerForm()
         return render(request, "add_customer.html",{'form': form})
+    
+def edit_customer(request, id):
+    customer = Customer.objects.get(id=id)
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, instance=customer)
+        if form.is_valid():
+            form.save()
+            return redirect('customer', customer.id)
+    else:
+        form = CustomerForm(instance=customer)
+    return render(request, 'edit_customer.html', {'form': form})
+
+
+
+                # Worksheet Views #
+                ###################
+def worksheet(request):
+    worksheets = Worksheet.objects.all()
+    return render(request, 'worksheet.html', {'worksheets': worksheets})
 
 def add_worksheet(request):
     if request.method =="POST":
@@ -56,6 +79,25 @@ def add_worksheet(request):
         form = WorksheetForm()
         return render(request, "add_worksheet.html",{'form': form})
 
+def edit_worksheet(request, id):
+    worksheet = Worksheet.objects.get(id=id)
+    if request.method == 'POST':
+        form = WorksheetForm(request.POST, instance=worksheet)
+        if form.is_valid():
+            form.save()
+            return redirect('worksheet', worksheet.id)
+    else:
+        form = WorksheetForm(instance=worksheet)
+    return render(request, 'edit_worksheet.html', {'form': form})
+    
+
+
+                # Service Views #
+                #################
+def service(request):
+    services = Service.objects.all()
+    return render(request, 'service.html', {'services': services})
+
 def add_service(request):
     if request.method =="POST":
         form = ServiceForm(request.POST)
@@ -66,6 +108,23 @@ def add_service(request):
         form = ServiceForm()
         return render(request, "add_service.html",{'form': form})
     
+def edit_service(request, id):
+    service = Service.objects.get(id=id)
+    if request.method == 'POST':
+        form = ServiceForm(request.POST, instance=service)
+        if form.is_valid():
+            form.save()
+            return redirect('service', service.id)
+    else:
+        form = ServiceForm(instance=service)
+    return render(request, 'edit_service.html', {'form': form})
+
+
+                # Stock Views #
+                ###############
+def stock(request):
+    stocks = Stock.objects.all()
+    return render(request, 'stock.html', {'stocks': stocks})
 
 def add_stock(request):
     if request.method =="POST":
@@ -76,3 +135,14 @@ def add_stock(request):
     else:
         form = StockForm()
         return render(request, "add_stock.html",{'form': form})
+    
+def edit_stock(request, id):
+    stock = Stock.objects.get(id=id)
+    if request.method == 'POST':
+        form = StockForm(request.POST, instance=stock)
+        if form.is_valid():
+            form.save()
+            return redirect('stock', stock.id)
+    else:
+        form = StockForm(instance=stock)
+    return render(request, 'edit_stock.html', {'form': form})
